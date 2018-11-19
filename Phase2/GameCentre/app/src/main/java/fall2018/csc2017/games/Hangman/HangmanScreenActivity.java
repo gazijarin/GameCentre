87e753bbebf0693ae355779a17e2ca4ca90d83ee
Gazi <gazijarin@gmail.com>
2018-11-18 19:20:10 -0500

    Added Hangman's GameScreenActivity

---
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/games/Hangman/HangmanScreenActivity.java
@@ -0,0 +1,169 @@
+package fall2018.csc2017.games.Hangman;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import fall2018.csc2017.games.Hangman.HangmanManager;
+import fall2018.csc2017.games.Hangman.HangmanActivity;
+import fall2018.csc2017.games.Hangman.HangmanComplexityActivity;
+import fall2018.csc2017.slidingtiles.R;
+
+public class HangmanScreenActivity extends AppCompatActivity {
+    /**
+     * The main save file.
+     */
+    public static String SAVE_FILENAME;
+    /**
+     * The board manager, if one exists as a save.
+     */
+    private HangmanManager hangmanManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String username = getIntent().getStringExtra("USERNAME");
+
+        hangmanManager = new HangmanManager("medium");
+//        SAVE_FILENAME = username + "_" + hangmanManager.getGameId() + "_save_file.ser";
+        loadFromFile(SAVE_FILENAME);
+
+        setContentView(R.layout.activity_starting_);
+        addStartButtonListener();
+        addLoadButtonListener();
+        addSaveButtonListener();
+    }
+
+    /**
+     * Activate the start button.
+     */
+    private void addStartButtonListener() {
+        Button startButton = findViewById(R.id.StartButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToNewGame();
+            }
+        });
+
+    }
+
+    /**
+     * Activate the load button.
+     */
+    private void addLoadButtonListener() {
+        Button loadButton = findViewById(R.id.LoadButton);
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFromFile(SAVE_FILENAME);
+                saveToFile(SAVE_FILENAME);
+                makeToastLoadedText();
+                switchToGame();
+            }
+        });
+    }
+
+    /**
+     * Activate the save button.
+     */
+    private void addSaveButtonListener() {
+        Button saveButton = findViewById(R.id.SaveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveToFile(SAVE_FILENAME);
+                makeToastSavedText();
+            }
+        });
+    }
+
+    /**
+     * Display that a game was saved successfully.
+     */
+    private void makeToastSavedText() {
+        Toast.makeText(this, "Game Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Display that a game was loaded successfully.
+     */
+    private void makeToastLoadedText() {
+        Toast.makeText(this, "Loaded Game", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadFromFile(SAVE_FILENAME);
+    }
+
+    /**
+     * Switch to the GameActivity view to play the game.
+     */
+    private void switchToGame() {
+        Intent tmp = new Intent(this, HangmanActivity.class);
+        saveToFile(SAVE_FILENAME);
+        startActivity(tmp);
+    }
+
+    /**
+     * Switch to the GameActivity view to start a new game.
+     */
+    private void switchToNewGame() {
+        Intent tmp = new Intent(this, HangmanComplexityActivity.class);
+        startActivity(tmp);
+    }
+
+    /**
+     * Load the board manager from fileName.
+     *
+     * @param fileName the name of the file
+     */
+    private void loadFromFile(String fileName) {
+
+        try {
+            InputStream inputStream = this.openFileInput(fileName);
+            if (inputStream != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream);
+                hangmanManager = (HangmanManager) input.readObject();
+                inputStream.close();
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        } catch (ClassNotFoundException e) {
+            Log.e("login activity", "File contained unexpected data type: " + e.toString());
+        }
+    }
+
+
+    /**
+     * Save the board manager to fileName.
+     *
+     * @param fileName the name of the file
+     */
+    public void saveToFile(String fileName) {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    this.openFileOutput(fileName, MODE_PRIVATE));
+            outputStream.writeObject(hangmanManager);
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+}
+
