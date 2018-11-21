317c742d7df8f44d9e03f4b5de0800b12e6a6076
Gazi <gazijarin@gmail.com>
2018-11-20 23:13:42 -0500

    Cleaned up Hangman.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/games/Hangman/Hangman.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/games/Hangman/Hangman.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-//todo: add getters, setters
 
 /**
  * A class that represents a Hangman game.
@@ -19,22 +18,13 @@ public class Hangman {
      */
     private int numCorr = 0;
     /**
-     * The Hangman body.
-     */
-    private HangmanBody hangmanBody;
-    /**
      * The current word being guessed.
      */
     private String currWord;
     /**
-     * The difficulty of the word; default set to medium.
-     */
-    private String difficulty = "medium";
-    /**
      * View of all the characters.
      */
     private TextView[] charViews;
-
 
     /**
      * Represents a hangman object with generic difficulty.
@@ -42,14 +32,9 @@ public class Hangman {
      * @param word the word to be guessed
      */
     Hangman(String word) {
-
         currWord = word;
-
         charViews = new TextView[currWord.length()];
-
         numChars = currWord.length();
-
-
     }
 
     /**
@@ -57,7 +42,7 @@ public class Hangman {
      *
      * @return the number of characters
      */
-    public int getNumChars() {
+    int getNumChars() {
         return numChars;
     }
 
@@ -66,54 +51,8 @@ public class Hangman {
      *
      * @return the number of characters
      */
-    public int getNumCorr() {
+    int getNumCorr() {
         return numCorr;
-    }
-
-
-    /**
-     * Returns the current word.
-     *
-     * @return the current word
-     */
-    public String getCurrWord() {
-        return currWord;
-    }
-
-    /**
-     * Set numChars to inputted numChars.
-     *
-     * @param numChars the number of characters
-     */
-    public void setNumChars(int numChars) {
-        this.numChars = numChars;
-    }
-
-    /**
-     * Set numCorr to inputted numCorr.
-     *
-     * @param numCorr the number of correct characters
-     */
-    public void setNumCorr(int numCorr) {
-        this.numCorr = numCorr;
-    }
-
-    /**
-     * Set currWord to inputted currWord.
-     *
-     * @param currWord the current word
-     */
-    public void setCurrWord(String currWord) {
-        this.currWord = currWord;
-    }
-
-    /**
-     * Set difficulty to inputted difficulty.
-     *
-     * @param difficulty the difficulty
-     */
-    public void setDifficulty(String difficulty) {
-        this.difficulty = difficulty;
     }
 
     /**
