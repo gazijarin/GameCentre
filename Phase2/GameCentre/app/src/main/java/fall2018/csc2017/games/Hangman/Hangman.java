d1a17e8079e09933c075851479fbc15ee5adf7f4
Gaurav <g.sharma0909@gmail.com>
2018-11-24 22:32:39 -0500

    Decoupled aspects of Hangman so that the game stands by itself

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/games/Hangman/Hangman.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/games/Hangman/Hangman.java
@@ -12,17 +12,29 @@ public class Hangman {
      */
     String currWord;
     /**
+     * A char array representing the parts of the word that have been revealed
+     */
+    private char[] revealedWord;
+
+    /**
      * The number of characters correctly guessed.
      */
     int numCorr = 0;
-    /**
-     * View of all the characters.
-     */
-    List<Character> chars;
+
     /**
      * The number of characters in current word.
      */
     private int numChars;
+
+    /**
+     * The total number of guesses for the hangman game.
+     */
+    public final int TOTAL_GUESSES = 6;
+
+    /**
+     * The current guess amount.
+     */
+    private int currentGuesses;
 
     /**
      * Represents a hangman object with generic difficulty.
@@ -31,17 +43,14 @@ public class Hangman {
      */
     Hangman(String word) {
         currWord = word;
-        chars = new ArrayList<Character>();
-        numChars = currWord.length();
-    }
+        currentGuesses = 0;
+        revealedWord = new char[word.length()];
 
-    /**
-     * Forms ArrayList of chars from currWord to be used by activity
-     */
-    void populateChars() {
-        for (int c = 0; c < currWord.length(); c++) {
-            chars.add(currWord.charAt(c));
+        for (int i = 0; i < revealedWord.length; i++) {
+            revealedWord[i] = '@';
         }
+
+        numChars = currWord.length();
     }
 
     /**
@@ -54,22 +63,59 @@ public class Hangman {
     }
 
     /**
-     * Returns the number of characters.
+     * Returns the number of characters correctly guessed.
      *
-     * @return the number of characters
+     * @return the number of characters correctly guessed.
      */
     int getNumCorr() {
         return numCorr;
     }
 
     /**
-     * Returns the number of characters.
+     * Returns the current word
      *
-     * @return the number of characters
+     * @return the current word
      */
     String getCurrWord() {
         return currWord;
     }
 
+    /**
+     * Makes a character visible in this hangman
+     *
+     * @param character the character to make visible
+     * @return whether the letter was found
+     */
+    boolean makeVisible(char character) {
+        boolean found = false;
+        for (int i = 0; i < currWord.length(); i++) {
+            if (Character.toLowerCase(currWord.charAt(i)) ==
+                    Character.toLowerCase(character)) {
+                revealedWord[i] = character;
+                found = true;
+            }
+        }
+        if (!found) {
+            currentGuesses++;
+        }
+        return found;
+    }
 
+    /**
+     * returns the char array representing the parts revealed of the word.
+     *
+     * @return the char array representing the parts revealed of the word.
+     */
+    char[] getRevealedWord() {
+        return revealedWord;
+    }
+
+    /**
+     * Returns how many guesses so far
+     *
+     * @return how many guesses so far
+     */
+    public int getCurrentGuesses() {
+        return currentGuesses;
+    }
 }
