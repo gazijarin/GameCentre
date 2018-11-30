c16746179da4d6a45fb78f51ba815c56ffe985a8
DemetreJou <demetre.jouras@gmail.com>
2018-11-29 22:01:27 -0500

    Added docstrings to FixedStack tests

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/games/SlidingTiles/FixedStackTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/games/SlidingTiles/FixedStackTest.java
@@ -2,20 +2,26 @@ package fall2018.csc2017.games.SlidingTiles;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class FixedStackTest {
 
+    /**
+     * Tests that pushing and poping works when below max size
+     */
     @Test
     public void testBelowMaxSize() {
         FixedStack<Integer> temp = new FixedStack<>(5);
         temp.push(6);
         temp.push(7);
-        assertTrue(temp.pop() == 7);
+        assertEquals(7, (int) temp.pop());
 
     }
 
-
+    /**
+     * Tests that pushing and poping works when pushing to above max size
+     */
     @Test
     public void testAboveMaxSize() {
         FixedStack<Integer> temp = new FixedStack<>(2);
@@ -28,6 +34,9 @@ public class FixedStackTest {
 
     }
 
+    /**
+     * Test that pushing works as expected to a max size of 0
+     */
     @Test
     public void testSizeZero() {
         FixedStack<Integer> temp = new FixedStack<>(0);
